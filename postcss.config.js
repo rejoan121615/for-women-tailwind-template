defdd81a1ba80a447e6aa75e7b278d09a1e35b9b
Mohd Rejoan <wearecrx@gmail.com>
2022-01-26 01:18:16 +0600

    css file minifyed and project complete

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        cssnano: env === "production" ? { preset: "default" } : false,
+    },
+};
